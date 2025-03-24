8a36e8589e330e5401eca35b1091d40abb2ad461
Benjamin Rains <rains.bp@gmail.com>
2025-03-24 15:54:58 -0400

    added the provider staging model

---
+++ models/staging/opendental/stg_opendental__provider.sql
@@ -0,0 +1,82 @@
+with source as (
+    select * from {{ source('opendental', 'provider') }}
+),
+
+staged as (
+    select
+        -- Primary key
+        "ProvNum" as provider_id,
+        
+        -- Provider identifiers
+        "Abbr" as provider_abbreviation,
+        "ItemOrder" as provider_item_order,
+        "LName" as provider_last_name,
+        "FName" as provider_first_name,
+        "MI" as provider_middle_initial,
+        "Suffix" as provider_suffix,
+        "PreferredName" as provider_preferred_name,
+        "CustomID" as provider_custom_id,
+        
+        -- Provider classifications
+        "FeeSched" as fee_schedule_id,
+        "Specialty" as specialty_id,
+        "ProvStatus" as provider_status,
+        "AnesthProvType" as anesthesia_provider_type,
+        "SchoolClassNum" as school_class_number,
+        "EhrMuStage" as ehr_mu_stage,
+        "ProvNumBillingOverride" as provider_billing_override_id,
+        
+        -- Provider identifiers and numbers
+        "SSN" as ssn,
+        "StateLicense" as state_license,
+        "DEANum" as dea_number,
+        "BlueCrossID" as blue_cross_id,
+        "MedicaidID" as medicaid_id,
+        "NationalProvID" as national_provider_id,
+        "CanadianOfficeNum" as canadian_office_number,
+        "EcwID" as ecw_id,
+        "StateRxID" as state_rx_id,
+        "StateWhereLicensed" as state_where_licensed,
+        
+        -- Taxonomy and classification
+        "TaxonomyCodeOverride" as taxonomy_code_override,
+        
+        -- UI and display properties
+        "ProvColor" as provider_color,
+        "OutlineColor" as outline_color,
+        "SchedNote" as schedule_note,
+        "WebSchedDescript" as web_schedule_description,
+        "WebSchedImageLocation" as web_schedule_image_location,
+        
+        -- Financial and goals
+        "HourlyProdGoalAmt" as hourly_production_goal_amount,
+        
+        -- Boolean flags (keeping as smallint to match source)
+        "IsSecondary" as is_secondary,
+        "IsHidden" as is_hidden,
+        "UsingTIN" as is_using_tin,
+        "SigOnFile" as has_signature_on_file,
+        "IsCDAnet" as is_cdanet,
+        "IsNotPerson" as is_not_person,
+        "IsInstructor" as is_instructor,
+        "IsHiddenReport" as is_hidden_report,
+        "IsErxEnabled" as is_erx_enabled,
+        
+        -- Dates
+        "Birthdate" as birth_date,
+        "DateTerm" as termination_date,
+        "DateTStamp" as record_updated_at,
+        
+        -- Relations
+        "EmailAddressNum" as email_address_id,
+        
+        -- Metadata
+        current_timestamp as dbt_created_at,
+        '{{ invocation_id }}' as dbt_pipeline_id,
+        '{{ this.name }}' as dbt_model,
+        '{{ this.schema }}' as dbt_schema
+
+    from source
+)
+
+select * from staged
