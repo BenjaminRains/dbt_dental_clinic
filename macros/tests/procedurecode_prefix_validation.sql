a3499073e2deb2fceaf6b07afeb40092f2730e30
Benjamin Rains <rains.bp@gmail.com>
2025-03-25 20:56:33 -0400

    proccode test to validate proc code prefixes

---
+++ macros/tests/procedurecode_prefix_validation.sql
@@ -0,0 +1,14 @@
+{% test validate_procedure_code_prefixes(model, column_name) %}
+
+with validation as (
+    select
+        {{ column_name }},
+        SUBSTRING({{ column_name }}, 1, 2) as code_prefix
+    from {{ model }}
+    where {{ column_name }} like 'D%'
+      and SUBSTRING({{ column_name }}, 1, 2) not in ('D0', 'D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9')
+)
+
+select * from validation
+
+{% endtest %} 
