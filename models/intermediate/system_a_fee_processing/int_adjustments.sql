b606c14c51b75c6a153341282657c75a30953446
Benjamin Rains <rains.bp@gmail.com>
2025-03-28 10:08:02 -0400

    adjustment CTE for intermediate system a models.

---
+++ models/intermediate/system_a_fee_processing/int_adjustments.sql
@@ -0,0 +1,43 @@
+-- int_procedure_adjustments.sql
+WITH ProcedureAdjustments AS (
+    SELECT
+        a.adjustment_id,
+        a.patient_id,
+        a.procedure_id,
+        a.provider_id,
+        a.clinic_id,
+        a.adjustment_amount,
+        a.adjustment_date,
+        a.procedure_date,
+        a.adjustment_category,
+        a.adjustment_size,
+        a.is_procedure_adjustment,
+        a.is_retroactive_adjustment,
+        a.is_provider_discretion,
+        a.is_employee_discount,
+        a.is_military_discount,
+        a.is_courtesy_adjustment,
+        
+        -- Link to procedure data
+        pc.procedure_code,
+        pc.procedure_description,
+        pc.procedure_fee,
+        pc.fee_schedule_id,
+        pc.standard_fee,
+        
+        -- Calculate adjusted fee
+        pc.procedure_fee + COALESCE(a.adjustment_amount, 0) AS adjusted_fee,
+        
+        -- Adjustment impact flag
+        CASE
+            WHEN ABS(a.adjustment_amount) / NULLIF(pc.procedure_fee, 0) > 0.5 THEN 'major'
+            WHEN ABS(a.adjustment_amount) / NULLIF(pc.procedure_fee, 0) > 0.1 THEN 'moderate'
+            ELSE 'minor'
+        END AS adjustment_impact
+        
+    FROM {{ ref('stg_opendental__adjustment') }} a
+    LEFT JOIN {{ ref('int_procedure_complete') }} pc
+        ON a.procedure_id = pc.procedure_id
+)
+
+SELECT * FROM ProcedureAdjustments
