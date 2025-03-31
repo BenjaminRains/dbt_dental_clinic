113dd40fa4b2f1a429acbc2818135d0e5bd8cf9b
Benjamin Rains <rains.bp@gmail.com>
2025-03-31 14:24:34 -0400

    updated macro to include is_hygiene, entered_by_user_id, userrname columns.

--- tests/staging/appt_broken_wo_procs.sql
+++ tests/staging/appt_broken_wo_procs.sql
@@ -3,7 +3,13 @@ select
     patient_id,
     appointment_datetime,
     appointment_status,
-    procedure_description
-from {{ ref('stg_opendental__appointment') }}
+    procedure_description,
+    is_hygiene,
+    entered_by_user_id,
+    username
+from "opendental_analytics"."public_staging"."stg_opendental__appointment" a
+left join "opendental_analytics"."public_staging"."stg_opendental__userod" u 
+    on a.entered_by_user_id = u.user_id
 where appointment_status = 5  -- Broken/Missed
   and (procedure_description is null OR TRIM(procedure_description) = '')
+  and is_hygiene = 0  -- Only look at non-hygiene appointments
