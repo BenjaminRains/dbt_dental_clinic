900cbfa6a25f1c92dc0e07902484a17c614eda6b
Benjamin Rains <rains.bp@gmail.com>
2025-04-07 12:16:31 -0400

    files won't be overwritten and if already present will bre skipped.

--- test_wget.bat
+++ test_wget.bat
@@ -5,5 +5,14 @@ set OUTPUT_DIR=docs\opendental_manual
 REM Create the output directory if it doesn't exist
 if not exist "%OUTPUT_DIR%" mkdir "%OUTPUT_DIR%"
 
-REM Download all URLs from the list
-wget -i docs\urls.txt -P "%OUTPUT_DIR%" --no-check-certificate -E -H -k -p -nc
+REM Download all URLs from the list with verbose output
+REM -c: continue partially downloaded files
+REM -nc: no clobber (don't overwrite existing files)
+REM -N: don't re-retrieve files unless newer than local
+REM -v: verbose output
+wget -i docs\urls.txt -P "%OUTPUT_DIR%" --no-check-certificate -E -H -k -p -nc -N -v
+
+REM Check if files were downloaded
+echo.
+echo Checking downloaded files:
+dir "%OUTPUT_DIR%"
