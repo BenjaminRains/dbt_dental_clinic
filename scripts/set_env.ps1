c34392fee09c835ba757dd9e3be03b890425a7ef
Benjamin Rains <rains.bp@gmail.com>
2025-04-11 09:55:26 -0400

    added environment loading scripts

---
+++ scripts/set_env.ps1
@@ -0,0 +1,8 @@
+# Read the .env file and set environment variables
+Get-Content .env | ForEach-Object {
+    if ($_ -match '^([^#][^=]+)=(.*)$') {
+        $name = $matches[1].Trim()
+        $value = $matches[2].Trim()
+        [Environment]::SetEnvironmentVariable($name, $value, 'Process')
+    }
+}
