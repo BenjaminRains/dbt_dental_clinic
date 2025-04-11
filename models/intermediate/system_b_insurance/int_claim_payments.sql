054d3b96df818689a761fb2cc27edb6519a49ead
Benjamin Rains <rains.bp@gmail.com>
2025-04-11 14:41:30 -0400

    added claim payment intermediate model

---
+++ models/intermediate/system_b_insurance/int_claim_payments.sql
@@ -0,0 +1,65 @@
+{{
+    config(
+        materialized='table',
+        schema='intermediate'
+    )
+}}
+
+with ClaimProc as (
+    select
+        claim_id,
+        procedure_id,
+        claim_payment_id,
+        fee_billed as billed_amount,
+        allowed_override as allowed_amount,
+        insurance_payment_amount as paid_amount,
+        write_off,
+        copay_amount as patient_responsibility
+    from {{ ref('stg_opendental__claimproc') }}
+    where claim_payment_id is not null
+),
+
+ClaimPayment as (
+    select
+        claim_payment_id,
+        check_amount,
+        check_date,
+        payment_type_id,
+        is_partial
+    from {{ ref('stg_opendental__claimpayment') }}
+),
+
+Final as (
+    select
+        -- Primary Key
+        cp.claim_id,
+        cp.procedure_id,
+        cp.claim_payment_id,
+
+        -- Financial Information
+        cp.billed_amount,
+        cp.allowed_amount,
+        cp.paid_amount,
+        cp.write_off as write_off_amount,
+        cp.patient_responsibility,
+
+        -- Payment Details
+        cpy.check_amount,
+        cpy.check_date,
+        cpy.payment_type_id,
+        cpy.is_partial,
+
+        -- Meta Fields
+        cpy.check_date as created_at,
+        cpy.check_date as updated_at
+
+    from ClaimProc cp
+    left join ClaimPayment cpy
+        on cp.claim_payment_id = cpy.claim_payment_id
+    -- Ensure the claim and procedure exist in int_claim_details
+    inner join {{ ref('int_claim_details') }} cd
+        on cp.claim_id = cd.claim_id
+        and cp.procedure_id = cd.procedure_id
+)
+
+select * from Final 
