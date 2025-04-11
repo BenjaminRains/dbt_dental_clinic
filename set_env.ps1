d9296527234c1e72a4912f8cd65ee9767071e84f
Benjamin Rains <rains.bp@gmail.com>
2025-04-11 09:08:37 -0400

    moved files

--- set_env.ps1
+++
@@ -1,8 +0,0 @@
-# Read the .env file and set environment variables
-Get-Content .env | ForEach-Object {
-    if ($_ -match '^([^#][^=]+)=(.*)$') {
-        $name = $matches[1].Trim()
-        $value = $matches[2].Trim()
-        [Environment]::SetEnvironmentVariable($name, $value, 'Process')
-    }
-}
