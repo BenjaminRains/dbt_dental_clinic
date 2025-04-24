4aca6cdbbcd9dcf0d0787998661b45e7e67dbcc7
Benjamin Rains <rains.bp@gmail.com>
2025-04-24 12:54:02 -0400

    updated source filter to include historical records that are still active

--- models/staging/opendental/stg_opendental__inssub.sql
+++ models/staging/opendental/stg_opendental__inssub.sql
@@ -4,7 +4,18 @@
 
 with source as (
     select * from {{ source('opendental', 'inssub') }}
-    where "SecDateTEdit" >= '2023-01-01'
+    where 
+        -- Include records modified in 2023+
+        "SecDateTEdit" >= '2023-01-01'
+        -- OR include historical records that are still active
+        OR (
+            "SecDateTEdit" < '2023-01-01'
+            AND (
+                -- Only include non-terminated or future-terminated records
+                "DateTerm" IS NULL 
+                OR "DateTerm" >= CURRENT_DATE
+            )
+        )
 ),
 
 renamed as (
@@ -28,7 +39,14 @@ renamed as (
         "AssignBen" as is_assign_benefits,
         "SubscriberID" as subscriber_external_id,
         "BenefitNotes" as benefit_notes,
-        "SubscNote" as subscriber_notes
+        "SubscNote" as subscriber_notes,
+        
+        -- Metadata
+        CASE 
+            WHEN "DateTerm" < CURRENT_DATE THEN 'TERMINATED'
+            WHEN "DateTerm" IS NULL THEN 'ACTIVE'
+            ELSE 'FUTURE_TERMINATED'
+        END as subscriber_status
 
     from source
 )
