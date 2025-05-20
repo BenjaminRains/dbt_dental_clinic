cec716c1c2c9a787c3272c9158cfb57a5c027cec
Benjamin Rains <rains.bp@gmail.com>
2025-05-20 09:38:09 -0400

    feat: add employer staging model and documentation
    - Create stg_opendental__employer.sql with standardized column naming
    - Add _stg_opendental__employer.yml with:
      - Model and column descriptions
      - Data quality tests for employer_num and employer_name
      - Documentation for all address and contact fields

---
+++ models/staging/opendental/stg_opendental__employer.sql
@@ -0,0 +1,28 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
+
+with source as (
+    select * from {{ source('opendental', 'employer') }}
+),
+
+renamed as (
+    select
+        -- Primary Key
+        "EmployerNum" as employer_num,
+        
+        -- Attributes
+        "EmpName" as employer_name,
+        "Address" as address,
+        "Address2" as address2,
+        "City" as city,
+        "State" as state,
+        "Zip" as zip,
+        "Phone" as phone
+
+    from source
+)
+
+select * from renamed
