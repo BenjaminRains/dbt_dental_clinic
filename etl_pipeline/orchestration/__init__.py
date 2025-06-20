bdcd46deab4c60912cd5e917b3a67941d6d97ed1
Benjamin Rains <rains.bp@gmail.com>
2025-06-20 13:50:57 -0400

    chore: removed deprecated legacy code

--- etl_pipeline/orchestration/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-ETL Pipeline orchestration components.
-"""
-from etl_pipeline.orchestration.pipeline_runner import PipelineRunner
-from etl_pipeline.orchestration.dependency_graph import DependencyGraph
-from etl_pipeline.orchestration.scheduler import PipelineScheduler
-
-__all__ = ['PipelineRunner', 'DependencyGraph', 'PipelineScheduler'] 
