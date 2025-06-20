526f9de8228c26b068e3290d92e9ff2c9734356f
Benjamin Rains <rains.bp@gmail.com>
2025-06-20 19:27:20 -0400

    chore: removed deprecated files following REFACTOING_PLAN.md

--- etl_pipeline/etl_pipeline/__main__.py
+++
@@ -1,6 +0,0 @@
-"""Main entry point when running as module."""
-import sys
-from etl_pipeline.cli.__main__ import __name__ as __main__
-
-if __name__ == '__main__':
-    sys.exit(0)
--- etl_pipeline/etl_pipeline/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Utility functions and classes for the ETL pipeline.
-"""
-from etl_pipeline.utils.notifications import NotificationConfig, NotificationManager
-
-__all__ = ['NotificationConfig', 'NotificationManager'] 
