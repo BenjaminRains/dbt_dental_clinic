0f3146cbca3560a660491eee752941abe31f12cc
Benjamin Rains <rains.bp@gmail.com>
2025-08-08 11:35:11 -0400

    fix: remove emplyee_entry_logs cte and add refactored standaradize_metadata_columns macro ref

--- dbt_dental_clinic_prod/models/staging/opendental/stg_opendental__employee.sql
+++ dbt_dental_clinic_prod/models/staging/opendental/stg_opendental__employee.sql
@@ -6,15 +6,6 @@ with source_data as (
     select * from {{ source('opendental', 'employee') }}
 ),
 
-employee_entry_logs as (
-    select 
-        "FKey" as employee_id,
-        min("EntryDateTime") as first_entry_datetime
-    from {{ source('opendental', 'entrylog') }}
-    where "FKeyType" = 0  -- Assuming 0 is the type for employee records
-    group by "FKey"
-),
-
 renamed_columns as (
     select
         -- Primary Key
@@ -37,15 +28,10 @@ renamed_columns as (
         {{ convert_opendental_boolean('"IsFurloughed"') }} as is_furloughed,
         {{ convert_opendental_boolean('"IsWorkingHome"') }} as is_working_home,
 
-        -- Metadata columns (custom implementation due to entrylog join)
-        current_timestamp as _loaded_at,
-        el.first_entry_datetime as _created_at,
-        current_timestamp as _updated_at,
-        null as _created_by_user_id
+        -- ETL and dbt tracking columns
+        {{ standardize_metadata_columns() }}
 
-    from source_data s
-    left join employee_entry_logs el
-        on s."EmployeeNum" = el.employee_id
+    from source_data
 )
 
 select * from renamed_columns
