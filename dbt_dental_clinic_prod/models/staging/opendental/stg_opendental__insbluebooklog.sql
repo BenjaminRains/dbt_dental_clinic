0dfc0a28f158c3ad9fd0087af33a71eb9503887f
Benjamin Rains <rains.bp@gmail.com>
2025-08-11 17:10:01 -0400

    fix: replaced "_date_created" with "_loaded_at"
    fix: removed _created_at, _updated_at from metadata columns

--- dbt_dental_clinic_prod/models/staging/opendental/stg_opendental__insbluebooklog.sql
+++ dbt_dental_clinic_prod/models/staging/opendental/stg_opendental__insbluebooklog.sql
@@ -7,7 +7,7 @@ with source_data as (
     select * from {{ source('opendental', 'insbluebooklog') }}
     where "DateTEntry" >= '2023-01-01'
     {% if is_incremental() %}
-        and "DateTEntry" > (select max(date_created) from {{ this }})
+        and "DateTEntry" > (select max(_loaded_at) from {{ this }})
     {% endif %}
 ),
 
@@ -29,11 +29,7 @@ renamed_columns as (
         {{ clean_opendental_date('"DateTEntry"') }} as date_created,
         
         -- Standardized metadata columns
-        {{ standardize_metadata_columns(
-            created_at_column='"DateTEntry"',
-            updated_at_column='"DateTEntry"',
-            created_by_column=none
-        ) }}
+        {{ standardize_metadata_columns() }}
         
     from source_data
 )
