5d73d84e2be377ef1791a07fe44e2380dc464c22
Benjamin Rains <rains.bp@gmail.com>
2025-08-12 11:43:44 -0400

    refactor: "LinkType"::integer
    caste LinkType to integer
    
    remove params from standardize_metadata_columns

--- dbt_dental_clinic_prod/models/staging/opendental/stg_opendental__patientlink.sql
+++ dbt_dental_clinic_prod/models/staging/opendental/stg_opendental__patientlink.sql
@@ -16,17 +16,13 @@ renamed_columns as (
         ]) }},
         
         -- Attributes
-        "LinkType" as link_type,
+        "LinkType"::integer as link_type,
         
         -- Date Fields
         {{ clean_opendental_date('"DateTimeLink"') }} as linked_at,
         
         -- Standardized metadata columns
-        {{ standardize_metadata_columns(
-            created_at_column='"DateTimeLink"',
-            updated_at_column='"DateTimeLink"',
-            created_by_column=none
-        ) }}
+        {{ standardize_metadata_columns() }}
 
     from source_data
 )
