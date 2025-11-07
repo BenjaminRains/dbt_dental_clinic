2f3efb15fdc4e79812c880c798111b681263050c
Benjamin Rains <rains.bp@gmail.com>
2025-11-07 13:40:58 -0500

    feat: added app.include_router(ar.router)

--- api/main.py
+++ api/main.py
@@ -2,7 +2,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from routers import patient, reports, appointment, provider, revenue, dbt_metadata
+from routers import patient, reports, appointment, provider, revenue, dbt_metadata, ar
 
 
 app = FastAPI(
@@ -27,6 +27,7 @@ app.include_router(appointment.router)
 app.include_router(provider.router)
 app.include_router(revenue.router)
 app.include_router(dbt_metadata.router)
+app.include_router(ar.router)
 
 @app.get("/")
 def read_root():
